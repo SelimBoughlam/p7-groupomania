06d236bfc292feb6f4ae5c4779d3d436d32a4d02
selim boughlam <selimboughlam@gmail.com>
2022-01-24 16:16:58 +0100

    -téléchargement des premières dépendances: express,dotenv,nodemon -création du fichier .gitignore : node_modules,.env -lancement du serveur

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// Imports
+const express = require("express");
+const app = express();
+require("dotenv").config({ path: "../.env" });
+
+// Serveur
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
+});
